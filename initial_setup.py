4fda1771ef1dce095f96ec7410e8d3fb7fe11e9f
ArranScaife <scaife.arran@gmail.com>
2023-04-13 08:23:36 -0400

    Add gitignore exceptions for necessary Carla files and include utility python scripts

--- initial_setup.py
+++ initial_setup.py
@@ -3,5 +3,13 @@ import random
 
 # Connect to the client and retrieve the world object
 client = carla.Client('localhost', 2000)
+client.load_world('Town07')
 world = client.get_world()
-client.load_world('Town05')
+level =  world.get_map()
+weather = world.get_weather()
+blueprint_library = world.get_blueprint_library()
+
+settings = world.get_settings()
+settings.synchronous_mode = True
+settings.fixed_delta_seconds = 0.05
+world.apply_settings(settings)
