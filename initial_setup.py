76afbcfa8b9effb324c08b8cb7cf8e1058e61a3f
ArranScaife <scaife.arran@gmail.com>
2023-04-13 08:01:25 -0400

    Add requirements and inital setup for client

---
+++ initial_setup.py
@@ -0,0 +1,7 @@
+import carla
+import random
+
+# Connect to the client and retrieve the world object
+client = carla.Client('localhost', 2000)
+world = client.get_world()
+client.load_world('Town05')
